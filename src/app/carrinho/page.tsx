dce399ce35d126d6f33de1856e66acb17bcf0104
Caio Roberto da Silva Vercosa <crsv@WINDOWS.CIN.UFPE.BR>
2024-07-10 16:40:24 -0300

    feat: start logic carrinho page

--- src/app/carrinho/page.tsx
+++ src/app/carrinho/page.tsx
@@ -1,9 +1,31 @@
+'use client' // Componente Cliente -> Manipulação de variáveis de estado
+
+import { useState, useEffect } from 'react'; 
+
 export default function Carrinho() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p>Carrinho</p>
+    <div className={`w-screen h-screen`}>
+      <div className="min-h-full w-full hide-scrollbar bg-background p-4">
+        
+        <div className={`rounded-md min-h-full w-full bg-green-800 border-2`}>
+          <div className={`w-full bg-green-100`}>
+            {`<-`}
+          </div>
+            
+          <div className={`md:flex w-full h-full`}>
+              <div className={`w-full h-full bg-red-800`}>
+                a
+              </div>
+
+              <div className={`w-full bg-blue-800`}>
+                b
+              </div>
+
+          </div>
+
+
+        </div>
       </div>
-    </main>
+    </div>
   );
 }
